7aad9d59b0d067efcb9fc068f4a1fa18707d3c77
Lawyer <lawyerh_01@yahoo.com>
2018-08-09 20:42:09 -0700

    first commit

---
+++ minimalCoins.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+namespace algoSite
+{
+    public class MinimalCoins 
+    {
+
+         public MinimalCoins()
+         {
+
+         }
+
+         public MinimalCoins(int[] coins, int amount)
+         {
+             
+         }
+    }
+}
---
+++ server.js
@@ -0,0 +1,18 @@
+"use strict";
+
+function minimalCoins(coins, amount){
+    if(isNaN(amount) || amount === null)
+    {
+       return "The amount must be a number";
+        
+    }
+
+    if(typeof(coins) !== 'object')
+    {
+        
+    }
+}
+
+console.log(minimalCoins(1, null));
+
+
